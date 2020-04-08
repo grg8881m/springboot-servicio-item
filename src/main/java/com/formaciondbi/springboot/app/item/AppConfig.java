75ce27d16978af521285acb849b054c71204610c
Harold Adrian Bolaños Rodriguez <habr0075@coomeva.com.co>
2020-04-08 08:23:07 -0500

    Conectando nuestros servicios como clientes Eureka

--- src/main/java/com/formaciondbi/springboot/app/item/AppConfig.java
+++ src/main/java/com/formaciondbi/springboot/app/item/AppConfig.java
@@ -1,12 +1,15 @@
 package com.formaciondbi.springboot.app.item;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
+	
 	@Bean("clienteRestTemplate")
+	@LoadBalanced
 	public RestTemplate clienteRestTemplate() {
 	return new RestTemplate();	
 	}
--- src/main/java/com/formaciondbi/springboot/app/item/models/services/ItemServiceImpl.java
+++ src/main/java/com/formaciondbi/springboot/app/item/models/services/ItemServiceImpl.java
@@ -21,7 +21,8 @@ public class ItemServiceImpl implements ItemService {
 	
 	@Override
 	public List<Item> findAll() {
-		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8501/listar", Producto[].class));
+		//List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://localhost:8501/listar", Producto[].class));
+		List<Producto> productos = Arrays.asList(clienteRest.getForObject("http://servicio-productos/listar", Producto[].class));
 		return productos.stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
 	}
 
@@ -29,7 +30,8 @@ public class ItemServiceImpl implements ItemService {
 	public Item findById(Long id, Integer cantidad) {
 		Map<String, String> pathVariables = new HashMap<String, String>();
 		pathVariables.put("id", id.toString());
-		Producto producto =  clienteRest.getForObject("http://localhost:8501/ver/{id}", Producto.class, pathVariables);
+		//Producto producto =  clienteRest.getForObject("http://localhost:8501/ver/{id}", Producto.class, pathVariables);
+		Producto producto =  clienteRest.getForObject("http://servicio-productos/ver/{id}", Producto.class, pathVariables);
 		return new Item(producto, cantidad);
 	}
 
