b5849f5ca876d5c0c0bdaf22511c185f99b3430d
Harold Adrian Bolaños Rodriguez <habr0075@coomeva.com.co>
2020-04-08 12:25:25 -0500

    Tolerancia de fallos y latencia con Hystrix

--- src/main/java/com/formaciondbi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formaciondbi/springboot/app/item/controllers/ItemController.java
@@ -1,5 +1,6 @@
 package com.formaciondbi.springboot.app.item.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formaciondbi.springboot.app.item.models.Item;
+import com.formaciondbi.springboot.app.item.models.Producto;
 import com.formaciondbi.springboot.app.item.models.services.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -24,8 +27,22 @@ public class ItemController {
 		return itemService.findAll();
 	}
 	
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
+	
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		item.setCantidad(cantidad);
+		Producto producto = new Producto();
+		producto.setId(id);
+		producto.setNombre("ITEM No Clasificado");
+		producto.setPrecio(0D);
+		producto.setCreado(new Date());
+		item.setProducto(producto);
+		return item;
+	}
 }
+
